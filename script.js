d9f1a2d310f1851895caf9e7a1821a7905ec8a01
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-06-27 22:48:35 -0400

    created outline for js implementations that need to be done

---
+++ script.js
@@ -0,0 +1,17 @@
+// Array that book objects are going to be stored into
+const myLibrary = []; 
+
+// Book objects
+function Book() {
+  // the constructor...
+}
+// Function that take user’s input and store the new book objects into an array
+function addBookToLibrary() {
+  // do stuff here
+}
+
+// Function for new book to be added with a form that states author, title, number of pages, and if it was read or not
+
+// Function for read/not read book status
+
+// Function for ability to remove book from list
