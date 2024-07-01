bdb5e214085410a08f9360022df5481dcb818568
tristanbaldeo <113404111+tristanbaldeo@users.noreply.github.com>
2024-06-30 20:08:46 -0400

    added open/close button functionality for modal window

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // Array that book objects are going to be stored into
 const myLibrary = []; 
 
+const modal = document.getElementById("modal")
+const modalForm = document.getElementById("modal-form")
+const buttons = document.getElementById("buttons")
+const closeButton = document.getElementById("close-button")
+const addButton = document.getElementById("add-button")
+const addBook = document.getElementById("book")
+const bookCard = document.querySelectorAll('.book-card')
+const bookTitle = document.querySelectorAll('.book-title')
+const bookAuthor = document.querySelectorAll('.book-author')
+const pageCount = document.querySelectorAll('.page-count')
+const bookButtons = document.querySelectorAll('.book-buttons')
+const read = document.querySelectorAll('.read')
+const remove = document.querySelectorAll('.remove')
+const formItem = document.querySelectorAll('.form-item')
+
 // Book objects
 function Book(title, author, pages, read) {
     this.title = title;
@@ -14,9 +29,18 @@ function addBookToLibrary() {
 }
 
 // Function for new book pop-up form to be added with a form that states author, title, number of pages, and if it was read or not
+addBook.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
 
 // Function that updates the web page with the display on the book (might be included in function above)
 
+
 // Function for read/not read book status when selected
+
 
 // Function for ability to remove book from list with remove button
